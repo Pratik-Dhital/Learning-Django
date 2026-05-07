d0ebbdbb6930ab956d5b35d21a913e23d83a0ac9
Pratik-Dhital <pratikdhital555@gmail.com>
2026-05-07 20:32:47 +0545

    added a search bar and search filter

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from first.views import first_fun, second_fun, contact, about_us
-from vegu.views import receipe, delete_receipe
+from vegu.views import receipe, delete_receipe, update_receipe
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -30,6 +30,7 @@ urlpatterns = [
     path('about-us/', about_us, name='about_us'),
     path('receipe/', receipe, name='receipe'),
     path('delete-receipe/<id>/', delete_receipe, name='delete-receipe'),
+    path('update-receipe/<id>/', update_receipe, name='update_receipe'),
     path('admin/', admin.site.urls),
 ]
 
--- my_project/vegu/views.py
+++ my_project/vegu/views.py
@@ -17,8 +17,25 @@ def receipe(request):
         ) 
         return redirect('/receipe/')
     queryset = Recipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset = queryset.filter(name__icontains = request.GET.get('search'))
+       
+
     context = {"receipe" : queryset}
     return render(request, "receipe.html", context)
+
+def update_receipe(request, id):
+    receipe = Recipe.objects.get(id=id)
+    if request.method == "POST":
+        receipe.name = request.POST.get('name')
+        receipe.description = request.POST.get('description')
+        if request.FILES.get('receipe_image'):
+            receipe.receipe_image = request.FILES.get('receipe_image')
+        receipe.save()
+        return redirect('/receipe/')
+    context = {'receipe': receipe}
+    return render(request, 'update_receipe.ht ml', context)
 
 def delete_receipe(request, id):
     queryset = Recipe.objects.get(id = id)
